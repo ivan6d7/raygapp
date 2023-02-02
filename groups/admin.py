e6ebcd82f2af53c67988149505f71bd2e46655a9
ivan6d7 <ivan7d6@gmail.com>
2023-02-02 10:25:57 +0200

    groups added

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import HabitGroup
+from django.contrib.auth.models import Group
 
 # Register your models here.
+
+admin.site.unregister(Group)
+admin.site.register(HabitGroup)
--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from groups.models import HabitGroup
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.CharField(max_length=500)
-    
+    creation_date = models.DateTimeField()
+    group = models.ForeignKey(HabitGroup, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
